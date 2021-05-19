cfa1b2d846e5324c6fb6537a829ef7806be6f7ba
abbascoder51@gmail.com <Time2code91>
2021-05-19 17:32:16 +0100

    - Added Property migration and model

---
+++ database/migrations/2021_05_19_161707_create_properties_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('county_id')->references('id')->on('counties');
+            $table->foreignId('country_id')->references('id')->on('countries');
+            $table->foreignId('town_id')->references('id')->on('towns');
+            $table->text('description');
+            $table->string('full_details_url');
+            $table->string('displayable_address');
+            $table->string('image_url');
+            $table->string('thumbnail_url');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->integer('number_of_bedrooms');
+            $table->integer('number_of_bathrooms');
+            $table->integer('price');
+            $table->foreignId('property_type_id')->references('id')->on('property_types');
+            $table->string('contract_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
